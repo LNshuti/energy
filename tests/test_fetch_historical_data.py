62b56bcf2604371ea1cf8f1b14e4e9eb3f7a7e37
LNshuti <nshutl0@sewanee.edu>
2024-11-13 18:34:33 -0600

    fix fetch_andplot and pass test

--- tests/test_fetch_historical_data.py
+++ tests/test_fetch_historical_data.py
@@ -1,16 +1,17 @@
 import pytest
 from src.main import fetch_historical_data
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 
 @patch('src.main.yf.Ticker')
 @patch('src.main.yf.download')
 def test_fetch_historical_data_exception(mock_yf_download, mock_yf_Ticker):
+    import pandas as pd 
     # Mock the yf.download function to raise an exception
     mock_yf_download.side_effect = Exception("Network Error")
     
     # Mock the yf.Ticker to avoid any side effects
-    mock_yf_Ticker.return_value = MagicMock()
+    mock_yf_Ticker.return_value = pd.DataFrame()
     
     ticker = 'AAPL'
     start_date = '2023-01-01'
@@ -24,8 +25,8 @@ def test_fetch_historical_data_exception(mock_yf_download, mock_yf_Ticker):
 @patch('src.main.yf.Ticker')
 @patch('src.main.yf.download')
 def test_fetch_historical_data_no_data(mock_yf_download, mock_yf_info):
-    from unittest.mock import MagicMock
-    mock_yf_download.return_value = MagicMock(empty=True)
+    import pandas as pd
+    mock_yf_download.return_value = pd.DataFrame()
     
     ticker = 'INVALID'
     start_date = '2023-01-01'
